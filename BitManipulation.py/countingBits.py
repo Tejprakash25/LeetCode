c54ce21bf185ac5b536cb39a8c9d67c46a723524
Tejprakash25 <tejprakashmirashi25@example.com>
2026-03-12 22:53:26 +0530

    Counting bits solved using bit trick relationship and single number part 3

---
+++ BitManipulation.py/countingBits.py
@@ -0,0 +1,14 @@
+"""
+338. Counting Bits
+Given an integer n, return an array ans of length n + 1 such that for each
+i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+"""
+
+class Solution:
+    def countBits(self, n):
+        ans = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            ans[i] = ans[i & (i - 1)] + 1
+
+        return ans
---
+++ BitManipulation.py/singleNumber3.py
@@ -0,0 +1,28 @@
+"""
+260. Single Number III
+Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+Example 1:
+Input: nums = [1,2,1,3,2,5]
+Output: [3,5]
+Explanation:  [5, 3] is also a valid answer."""
+
+class Solution(object):
+    def singleNumber(self, nums):
+        xor = 0
+
+        for num in nums:
+            xor ^= num
+        
+        mask = xor & -xor
+
+        a = 0
+        b = 0
+
+        for num in nums:
+            if mask & num:
+                a ^= num
+            else:
+                b ^= num
+            
+        return [a, b]
+        
