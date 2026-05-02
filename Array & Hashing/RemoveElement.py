f5d914146fc583d09b4ec5d3ad8815b6f61685c6
Tejprakash25 <tejprakashmirashi25@gmail.com>
2026-05-02 23:40:18 +0530

    solved leetcode problem 35

--- Array & Hashing/RemoveElement.py
+++ Array & Hashing/RemoveElement.py
@@ -1,9 +1,7 @@
 class Solution(object):
-    def removeElement(self, nums, val):
-        a = 0
-        for i in nums:
-            if i != val:
-                nums[a] = i
-                a += 1
-        
-        return a
+    def searchInsert(self, nums, target):
+        for i, v in enumerate(nums):
+            if target <= v:
+                return i
+            
+        return len(nums)
---
+++ Array & Hashing/searchInsertPosition.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        for i, v in enumerate(nums):
+            if target <= v:
+                return i
+        return len(nums)
+    
+# Without using built-in functions
+class Solution:
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
