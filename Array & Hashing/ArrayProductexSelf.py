b399279885fbfbea568a4c56d67b89cbb8817269
Tejprakash25 <tejprakashmirashi25@gmail.com>
2026-05-06 22:11:07 +0530

    solved problem, where multiplied array except self

---
+++ Array & Hashing/ArrayProductexSelf.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        result = [1] * n
+
+        for i in range(1, n):
+            result[i] = result[i - 1] * nums[i - 1]
+
+        right = 1
+        for i in range(n - 1, -1, -1):
+            result[i] = result[i] * right
+            right *= nums[i]
+
+        return result
