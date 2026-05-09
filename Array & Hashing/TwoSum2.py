9cff161a840cfd37e7e4929734f94e7ce6cd0c3e
Tejprakash25 <tejprakashmirashi25@gmail.com>
2026-05-09 21:42:33 +0530

    Two Sum 2 - Input Array is Sorted

---
+++ Array & Hashing/TwoSum2.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            cur_sum = numbers[left] + numbers[right]
+        
+            if cur_sum == target:
+                return [left+1, right+1]
+
+            elif cur_sum < target:
+                left+=1
+
+            else:
+                right-=1
