f9be6f4d31e5d75fa90f8ecf4f4c2b1100a5b0b9
Tejprakash25 <tejprakashmirashi25@gmail.com>
2026-05-18 23:29:51 +0530

    Koko Eating Bananas

---
+++ BinarySearch/KokoEatingBananas.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+
+        left = 1
+        right = max(piles)
+
+        result = right
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            hours = 0
+
+            for pile in piles:
+
+                hours += (pile + mid - 1) // mid
+
+            if hours <= h:
+
+                result = mid
+                right = mid - 1
+
+            else:
+                left = mid + 1
+
+        return result
