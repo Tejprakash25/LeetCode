462ead2e9d64367e5f66bc94e503a5b5a53105a0
Tejprakash25 <tejprakashmirashi25@gmail.com>
2026-05-25 22:46:52 +0530

    Advance Binary Search + Data Structure Combination problem

---
+++ BinarySearch/TimeBasedKeyValue.py
@@ -0,0 +1,37 @@
+class TimeMap(object):
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key, value, timestamp):
+
+        if key not in self.store:
+            self.store[key] = []
+
+        self.store[key].append([timestamp, value])
+
+    def get(self, key, timestamp):
+
+        if key not in self.store:
+            return ""
+
+        values = self.store[key]
+
+        left, right = 0, len(values) - 1
+
+        result = ""
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            if values[mid][0] <= timestamp:
+
+                result = values[mid][1]
+                left = mid + 1
+
+            else:
+
+                right = mid - 1
+
+        return result
