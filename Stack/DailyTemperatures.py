1eaa643005cd7599935523fd288bd120b48442a1
Tejprakash25 <tejprakashmirashi25@gmail.com>
2026-05-31 23:59:20 +0530

    LC 739 : Daily Temperatures solved

---
+++ Stack/DailyTemperatures.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+
+        n = len(temperatures)
+
+        result = [0] * n
+
+        stack = []
+
+        for i in range(n):
+
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+
+                idx = stack.pop()
+
+                result[idx] = i - idx
+
+            stack.append(i)
+
+        return result
