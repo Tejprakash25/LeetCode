8a925bbfacd64eb8ac72fcfbe9f64f7ccaee15f6
Tejprakash25 <tejprakashmirashi25@gmail.com>
2026-06-04 23:38:11 +0530

    LC : 42 solved

---
+++ Stack/TrappingRainWater.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def trap(self, height):
+        left, right = 0, len(height) - 1
+        leftMax = rightMax = 0
+        water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                leftMax = max(leftMax, height[left])
+                water += leftMax - height[left]
+                left += 1
+
+            else:
+                rightMax = max(rightMax, height[right])
+                water += rightMax - height[right]
+                right -= 1
+
+        return water
