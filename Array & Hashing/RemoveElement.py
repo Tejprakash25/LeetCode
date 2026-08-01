6af91ea44ad8f3196845989cdb6ca30a4f173140
Tejprakash Mirashi <tejprakashmirashi25@gmail.com>
2026-08-01 23:22:05 +0530

    Update and rename RemoveElement.py to SearchInsertPosition.py

--- Array & Hashing/RemoveElement.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        for i, v in enumerate(nums):
-            if target <= v:
-                return i
-            
-        return len(nums)
