5530905f21240e80002a0e8cb75e605464bade70
Tejprakash25 <tejprakashmirashi25@gmail.com>
2026-08-14 23:48:49 +0530

    solved a LC problem after so many days, its LC:6 Zigzag Conversion

---
+++ Array & Hashing/ZigzagConversion.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        row, direction = 0, 1
+
+        for ch in s:
+            rows[row] += ch
+
+            if row == 0:
+                direction = 1
+            elif row == numRows - 1:
+                direction = -1
+
+            row += direction
+
+        return ''.join(rows)
